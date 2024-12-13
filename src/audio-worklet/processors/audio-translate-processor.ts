18106087e955c321426672d10a4206b5e186ca71
mengen.dai <mengen.dai@advantest.com>
2024-12-13 10:55:58 +0800

    feat(audio-worklet): 添加人声检测功能
    - 在 AudioTranslateProcessor 中添加了 fftSize、threshold 和 buffer 属性
    - 实现了基本的人声检测算法，通过计算音频信号的平均音量来判断是否有声音
    - 设置了阈值，当平均音量超过阈值时认为有人声，触发 postMessage 发送音频数据
    - 添加了控制台日志输出，用于调试和验证人声检测效果

--- src/audio-worklet/processors/audio-translate-processor.ts
+++ src/audio-worklet/processors/audio-translate-processor.ts
@@ -1,9 +1,29 @@
 class AudioTranslateProcessor extends AudioWorkletProcessor {
+    fftSize: number;
+    threshold: number;
+    buffer: Float32Array;
     constructor() {
         super();
+        this.fftSize = 128;
+        this.threshold = 0.1;
+        this.buffer = new Float32Array(this.fftSize);
     }
     process(inputs: Float32Array[][]): boolean {
-        this.port.postMessage(inputs[0][0]);
+        // inputs是接收到的音频数据输出
+        const inputChannel = inputs[0][0]; // 只用第一个通道
+        // TODO 下面的人声识别时临时写的，后续需要考虑选择其他的方法，目前暂时不做考虑
+        let sum = 0;
+        for (let i = 0; i < this.fftSize; i++) {
+            this.buffer[i] = inputChannel[i];
+            sum += this.buffer[i] * this.buffer[i];
+        }
+        const averageVolume = sum / this.fftSize;
+        if (averageVolume > this.threshold) {
+            console.log("Voice detected");
+            this.port.postMessage(inputs[0][0]);
+        } else {
+            console.log("No voice detected");
+        }
         return true;
     }
 }
