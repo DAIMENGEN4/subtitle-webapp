4fb9e61043c59560f03af65fb1ee7622f601bb5f
mengen.dai <mengen.dai@advantest.com>
2024-12-17 12:46:52 +0800

    feat(audio-worklet): 实现音频重采样功能
    - 添加重采样方法，将输入音频数据转换为目标采样率
    - 实现帧处理逻辑，确保输出符合目标帧大小
    - 优化音频数据处理流程，提高处理器兼容性

--- src/audio-worklet/processors/audio-translate-processor.ts
+++ src/audio-worklet/processors/audio-translate-processor.ts
@@ -1,12 +1,72 @@
 class AudioTranslateProcessor extends AudioWorkletProcessor {
-    constructor() {
+    private inputBuffer: Array<number> = [];
+    private readonly targetFrameSize: number = 320;
+    private readonly targetSampleRate: number = 16000;
+    private readonly nativeSampleRate: number = sampleRate;
+
+    constructor(options: AudioWorkletNodeOptions) {
         super();
+        this.targetFrameSize = options.processorOptions.targetFrameSize;
     }
+
     process(inputs: Float32Array[][]): boolean {
         // inputs是接收到的音频数据输出
-        const inputChannel = inputs[0][0]; // 只用第一个通道
-        this.port.postMessage(inputChannel);
+        const arrays = inputs[0][0]; // 只用第一个通道
+        const frames = this.resample(arrays);
+        for (const frame of frames) {
+            this.port.postMessage(frame);
+        }
         return true;
     }
+
+    private resample(audioFrame: Float32Array): Float32Array[] {
+        const outputFrames: Array<Float32Array> = []
+        for (const sample of audioFrame) {
+            this.inputBuffer.push(sample);
+        }
+        while (this.hasEnoughData()) {
+            const outputFrame = this.createOutputDataFrame();
+            outputFrames.push(outputFrame);
+        }
+        return outputFrames;
+    }
+
+    private hasEnoughData(): boolean {
+        return (
+            (this.inputBuffer.length * this.targetSampleRate) /
+            this.nativeSampleRate >=
+            this.targetFrameSize
+        )
+    }
+
+    private createOutputDataFrame(): Float32Array {
+        const outputFrame = new Float32Array(this.targetFrameSize)
+        let outputIndex = 0
+        let inputIndex = 0
+        while (outputIndex < this.targetFrameSize) {
+            let sum = 0
+            let num = 0
+            while (
+                inputIndex <
+                Math.min(
+                    this.inputBuffer.length,
+                    ((outputIndex + 1) * this.nativeSampleRate) /
+                    this.targetSampleRate
+                )
+                ) {
+                const value = this.inputBuffer[inputIndex]
+                if (value !== undefined) {
+                    sum += value
+                    num++
+                }
+                inputIndex++
+            }
+            outputFrame[outputIndex] = sum / num
+            outputIndex++
+        }
+        this.inputBuffer = this.inputBuffer.slice(inputIndex)
+        return outputFrame
+    }
 }
+
 registerProcessor("AudioTranslateProcessor", AudioTranslateProcessor);
