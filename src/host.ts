8f0befaac87284f34a87ac1dda9468dae478a742
mengen.dai <mengen.dai@advantest.com>
2025-01-20 11:02:07 +0800

    refactor(host): 更新主机名配置
    - 将主机名配置从 develop_hostname改为 intranet_hostname，用于内部网络环境
    - 注释掉 internet_hostname 以备将来使用

--- src/host.ts
+++ src/host.ts
@@ -1,3 +1,3 @@
-const develop_hostname = "https://subtitleai.advantest.com:39447";
-// const release_hostname = "https://smartworkplace.advantest.com.cn:9461";
-export const hostname = develop_hostname;
+// const internet_hostname = "https://smartworkplace.advantest.com.cn:9461";
+const intranet_hostname = "https://subtitleai.advantest.com:39447";
+export const hostname = intranet_hostname;
