4a3640e110222b4a1f71b9fbce0dd133c441cbd4
mengen.dai <mengen.dai@advantest.com>
2025-01-20 16:59:46 +0800

    refactor(src): 移除未使用的主机名变量
    - 删除了未使用的 internet_hostname 变量
    - 保留了 intranet_hostname 变量并将其导出

--- src/host.ts
+++ src/host.ts
@@ -1,3 +1,2 @@
-// const internet_hostname = "https://smartworkplace.advantest.com.cn:9461";
 const intranet_hostname = "https://subtitleai.advantest.com:39447";
 export const hostname = intranet_hostname;
